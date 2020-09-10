746c75b01febd1c847af233e24dff85889e0152e
vincenzot <vincenzo.timmel@students.fhnw.ch>
2020-09-10 22:42:23 +0200

    *LUL

--- cc-nn.py
+++ cc-nn.py
@@ -6,22 +6,30 @@ nn-cc model on it and evaluates it.
 from get_data import get_data_from_exchange
 from parameters import api_key
 import pandas as pd
+import numpy as np
 from fag import create_volume_bars, apply_triple_bar_method
 from mlfinlab.features.fracdiff import frac_diff_ffd, plot_min_ffd
 import plotly.graph_objects as go
+from keras.models import Sequential
+from keras.layers import Dense
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score, plot_roc_curve
 
 
-def add_trace_to_fig(fig, df):
+def add_trace_to_fig(fig, _df, max_bars=5000):
     """
+    :param max_bars:
     :param df: Dataframe with open, close, high, low, top_bar and low_bar
     :param fig: Initiated ploty-fig
-    :return: 
+    :return:
     """
+    df = _df.copy()
+    df = df.tail(max_bars)
     fig.add_trace(
         go.Scatter(
             mode='markers',
             x=df.index,
-            y=df['top_bar'] * df['close'] + df['top_bar'] * 1.2,
+            y=np.round(df['top_bar']) * df['close'] + np.round(df['top_bar']) * 1.2,
             marker=dict(
                 color='green',
                 size=2,
@@ -36,8 +44,8 @@ def add_trace_to_fig(fig, df):
     fig.add_trace(
         go.Scatter(
             mode='markers',
-            x=df['date_time'],
-            y=df['bot_bar'] * df['close'] - df['bot_bar'] * 1.2,
+            x=df.index,
+            y=np.round(df['bot_bar']) * df['close'] - np.round(df['bot_bar']) * 1.2,
             marker=dict(
                 color='red',
                 size=2,
@@ -52,14 +60,54 @@ def add_trace_to_fig(fig, df):
     return fig
 
 
+def _frac_diff_ffd(_df, d):
+    """
+    :param df: Dataset to differentiate with open, close, high, low, top_bar, low_bar
+    :param d: differentiate value
+    :return: differentiated price-values with the top_bar and low_bar readded
+    """
+    df = _df.copy()
+    labels = df[['top_bar', 'bot_bar']]
+    df = df[['open', 'high', 'low', 'close']]
+    df = frac_diff_ffd(df, d)
+    return df.join(labels)
+
+
+def add_past_rows(df_, period=7):
+    """
+    :param df: Dataframe with open, high, low, close, top_bar, bot_bar
+    :param period: How much of the past should be added to a row
+    :return: Dataframe with memory.
+    """
+    df = df_.copy()
+    labels = df[['top_bar', 'bot_bar']]
+    df = df[['open', 'high', 'low', 'close']]
+    df_unshifted = df.copy()
+    for i in range(1, period):
+        df = df.join(df_unshifted.shift(periods=i), how='inner', rsuffix='_' + str(i))
+    return df.join(labels).dropna()
+
+
+def calc_nr_neuros(df_differentiated_memory, output_neurons, alpha=2):
+    """
+    This function calculates with pi * thumb how many neuros there should be in a hidden layer.
+    https://stats.stackexchange.com/questions/181/how-to-choose-the-number-of-hidden-layers-and-nodes-in-a-feedforward-neural-netw
+    :param df_differentiated_memory: df which will be given to the nn-cc
+    :param alpha: parameter to change the number of neurons in the hidden layer
+    :return: int
+    """
+    sample_size, input_dim = df_differentiated_memory.shape
+    return int(sample_size / (alpha * (input_dim + output_neurons)))
+
+
 exchange = 'bitmex'
 exchange_parameters = api_key
 symbol = 'BTC/USD'
 symbol_path = symbol.replace('/', '-')
 tf = '1m'
-file_name = 'resources/Bitmex_{}_{}.parquet'.format(symbol_path, tf)
+file_name = 'resources/{}_{}_{}.parquet'.format(exchange, symbol_path, tf)
 limit = 1000
-starting_from = '2017-01-10 00:00:00Z'
+starting_from = '2018-01-10 00:00:00Z'
 header = ['date_time', 'open', 'high', 'low', 'close', 'volume']
 
 # Gets the newest data and saves it into a parquet.
@@ -67,6 +115,7 @@ get_data_from_exchange(symbol, symbol_path, starting_from, tf, file_name, limit,
                        exchange_parameters)
 
 df = pd.read_parquet(file_name)
+df = pd.read_parquet('resources/ETH-USDT.parquet')
 """
 PREPROCESSING
 """
@@ -80,35 +129,97 @@ if not duplicate_values.empty:
 
 print("Number of minute bars: {}".format(df.shape[0]))
 df_volume_bars = create_volume_bars(df)
+print("1 df_volume_bars.columns: ", df_volume_bars.columns)
 print("Number of volume bars: {}".format(df_volume_bars.shape[0]))
 df_volume_bars_labeled = apply_triple_bar_method(df_volume_bars, atr_mult=2.8, nr_of_bars_for_vertical=13,
                                                  period_atr=7)
 # Create plot to see the labels
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=df_volume_bars_labeled.index,
-                         y=df_volume_bars_labeled['close'],
+df_volume_bars_labeled_plot = df_volume_bars_labeled.tail(5000)
+fig.add_trace(go.Scatter(x=df_volume_bars_labeled_plot.index,
+                         y=df_volume_bars_labeled_plot['close'],
                          mode='lines',
                          name='lines'))
-fig = add_trace_to_fig(fig, df_volume_bars_labeled)
+fig = add_trace_to_fig(fig, df_volume_bars_labeled_plot)
 fig.write_html("plots/df_volume_bars_labeled.html")
 #
-# Save the labels and only keep the values which have to be differentiated
-labels = df_volume_bars_labeled[['top_bar', 'bot_bar']]
-X = df_volume_bars_labeled[['open', 'close', 'high', 'low', 'sum_of_candles']]
 
 # Plotting the graph to find the minimum value to use for the fractional differentiation (Where the line crosses the
 # 0.95 mark).
-fig = plot_min_ffd(X)
+fig = plot_min_ffd(df_volume_bars_labeled[['open', 'high', 'low', 'close']])
 fig.axhline(y=0.95, color='r', linestyle='--')
 fig.figure.savefig("plots/plot_min_ffd.png", bbox_inches='tight')
 
-# Deriving the fractionally differentiated features
-df_differentiated = frac_diff_ffd(X, 0.34)
+# Deriving the fractionally differentiated features. Creates a plot to check out the data
+df_differentiated = _frac_diff_ffd(df_volume_bars_labeled, 0.30)
+df_differentiated_plot = df_differentiated.tail(5000)
 
-fig = go.Figure(data=[go.Candlestick(x=df_differentiated.index,
-                                     open=df_differentiated['open'],
-                                     high=df_differentiated['high'],
-                                     low=df_differentiated['low'],
-                                     close=df_differentiated['close'])])
-
+fig = go.Figure(data=[go.Candlestick(x=df_differentiated_plot.index,
+                                     open=df_differentiated_plot['open'],
+                                     high=df_differentiated_plot['high'],
+                                     low=df_differentiated_plot['low'],
+                                     close=df_differentiated_plot['close'])])
 fig.write_html("plots/df_differentiated.html")
+
+df_differentiated_memory = add_past_rows(df_differentiated, 13)
+
+# We create an array to predict here for the softmax fuction: [1, 0, 0] to go long, [0, 1, 0] for short, [0,0,1] for no trade.
+y = df_differentiated_memory[['top_bar', 'bot_bar']]
+X = df_differentiated_memory.drop(columns=['top_bar', 'bot_bar'])
+# We also need, because we are using softmax, a label to tell it not to trade
+y['no_trade'] = np.where(y['top_bar'] + y['bot_bar'] == 0, 1, 0)
+
+# Marco Lopez del Prado said that we should have a time gap between test and train. This is what we do here.
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, shuffle=False)
+print(X_valid.tail())
+X_train.to_csv('X_train.csv')
+y_train.to_csv('y_train.csv')
+
+# Parameter for the model
+input_dim = X.shape[1]
+output_dim = 3
+nr_of_neuros = calc_nr_neuros(df_differentiated_memory, output_dim, 2)
+
+# Keras Model
+model = Sequential()
+model.add(Dense(input_dim + 1, input_dim=input_dim, activation='relu'))
+model.add(Dense(nr_of_neuros, activation='relu'))
+model.add(Dense(nr_of_neuros, activation='relu'))
+model.add(Dense(nr_of_neuros, activation='relu'))
+model.add(Dense(nr_of_neuros, activation='relu'))
+model.add(Dense(nr_of_neuros, activation='relu'))
+model.add(Dense(output_dim, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['categorical_accuracy'])
+model.summary()
+# Run keras Model
+history = model.fit(
+    X_train,
+    y_train,
+    batch_size=1024,
+    epochs=150,
+    # We pass some validation for
+    # monitoring validation loss and metrics
+    # at the end of each epoch
+    validation_data=(X_valid, y_valid),
+)
+
+# Evaluate Model
+pred_ = pd.DataFrame(model.predict(X_valid), columns=['top_bar', 'bot_bar', 'no_trade'])
+print("Prediction description: ", pred_.describe().apply(lambda s: s.apply(lambda x: format(x, 'g'))))
+print(pred_.describe().apply(lambda s: s.apply(lambda x: format(x, 'g'))).tail(10))
+
+len_ = np.minimum(pred_.shape[0], 5000)
+pred_ = pred_.tail(len_)
+pred_.index = df_volume_bars.tail(len_).index
+print("df_volume_bars.columns: ", df_volume_bars.columns)
+df_predicted = df_volume_bars.join(pred_, how='inner')
+print("df_predicted: ", df_predicted.tail(10))
+fig = go.Figure()
+df_predicted_plot = df_predicted.tail(len_)
+fig.add_trace(go.Scatter(x=df_predicted_plot.index,
+                         y=df_predicted_plot['close'],
+                         mode='lines',
+                         name='lines'))
+
+fig = add_trace_to_fig(fig, df_predicted, len_)
+fig.write_html("plots/df_predicted.html")
--- fag.py
+++ fag.py
@@ -1,28 +1,25 @@
 """
-This module stands for "features automatically generated". It
+This module stands for "features automatically generated". It generates features automatically.
 """
 
-import mlfinlab
 import numpy as np
-import pandas as pd
 
 
-def get_daily_volume(df, days_for_average=14, ratio=1 / 50):
+def get_daily_volume(_df, days_for_average=14, ratio=1 / 50):
     """
     :param df: candles with open, close, high, low and volume
     :param days_for_average: amount of days to get the daily averange volume
     :param ratio: ratio of daily volume to create one candle. Bro marco lopez de prado says a nice ratio is 1/50
     :returns: the optimal threshold of volume to create a volume bar
     """
+    df = _df.copy()
     df = df.resample('D').sum()
-    # Drop last entry because maybe it's an incomplete day. Documentation and google is unclear.
-    df = df[:-1]
     volume_threshold = df.tail(days_for_average)['volume'].mean() * ratio
     print("{} of daily {}-days average volume equals to {}.".format(ratio, days_for_average, volume_threshold))
     return volume_threshold
 
 
-def create_volume_bars(df, volume_for_new_candle=None):
+def create_volume_bars(_df, volume_for_new_candle=None):
     """
     :param volume_for_new_candle:
     :param df: candles with index (as date_time), open, close, high, low and volume
@@ -30,9 +27,9 @@ def create_volume_bars(df, volume_for_new_candle=None):
     bro marco lopez de prado
     :returns: volume bars (without the unfinished volume bar)
     """
+    df = _df.copy()
     if not volume_for_new_candle:
         volume_for_new_candle = get_daily_volume(df)
-    index_datetime = df['date_time']
     df['cum_volume'] = df.volume.cumsum()
     df['nr_of_candle'] = np.floor_divide(df['cum_volume'], volume_for_new_candle)
     df['volume_in_candle'] = df['cum_volume'] - df['nr_of_candle'] * volume_for_new_candle
@@ -51,7 +48,7 @@ def create_volume_bars(df, volume_for_new_candle=None):
     return df_grouped
 
 
-def reach_bar(df, atr_mult, nr_of_bars_for_vertical, which='top'):
+def reach_bar(_df, atr_mult, nr_of_bars_for_vertical, which='top'):
     """
     :param df: volume_bars with index (as date_time), open, close, high, low, atr and volume
     :param atr_mult: multiplier for the atr
@@ -59,6 +56,7 @@ def reach_bar(df, atr_mult, nr_of_bars_for_vertical, which='top'):
     :param which: which barrier to create
     :return: dataframe with a 1 if a close-price will reach the top/bot bar in the next <nr_of_bars_for_vertical> bars.
     """
+    df = _df.copy()
     if which == 'top':
         return np.where(df['close'].iloc[::-1].rolling(nr_of_bars_for_vertical, min_periods=0).max().iloc[::-1] >
                         df['close'] + df['ATR'] * atr_mult, 1, 0)
@@ -69,31 +67,33 @@ def reach_bar(df, atr_mult, nr_of_bars_for_vertical, which='top'):
         print("Please use only 'top' or 'bot' as parameter")
 
 
-def will_reach_top_bar(df, atr_mult, nr_of_bars_for_vertical):
+def will_reach_top_bar(_df, atr_mult, nr_of_bars_for_vertical):
     """
     :param df: volume_bars with index (as date_time), open, close, high, low, atr and volume
     :param atr_mult: multiplier for the atr
     :param nr_of_bars_for_vertical: after how many bars the vertical bar will be produced.
     :return: series with information if the bar will reach the top bar in less than <nr_of_bars_for_vertical> bars
     """
+    df = _df.copy()
     top_bars_reached = reach_bar(df, atr_mult, nr_of_bars_for_vertical, 'top')
     print("Price will pass a top bar {} times in this data.".format(np.sum(top_bars_reached)))
     return top_bars_reached
 
 
-def will_reach_bot_bar(df, atr_mult, nr_of_bars_for_vertical):
+def will_reach_bot_bar(_df, atr_mult, nr_of_bars_for_vertical):
     """
     :param df: volume_bars with index (as date_time), open, close, high, low, atr and volume
     :param atr_mult: multiplier for the atr
     :param nr_of_bars_for_vertical: after how many bars the vertical bar will be produced.
     :return: series with information if the bar will reach the bot bar in less than <nr_of_bars_for_vertical> bars
     """
+    df = _df.copy()
     bot_bars_reached = reach_bar(df, atr_mult, nr_of_bars_for_vertical, 'bot')
     print("Price will pass a bot bar {} times in this data.".format(np.sum(bot_bars_reached)))
     return bot_bars_reached
 
 
-def apply_triple_bar_method(df, atr_mult=2.8, nr_of_bars_for_vertical=13,
+def apply_triple_bar_method(_df, atr_mult=2.8, nr_of_bars_for_vertical=13,
                             period_atr=7):
     """
     :param df: dataframe with volume bars with open, close, high and low.
@@ -102,6 +102,7 @@ def apply_triple_bar_method(df, atr_mult=2.8, nr_of_bars_for_vertical=13,
     :param period_atr:
     :return: dataframe with volumebars, open high low close, atr and top bar and bot bar
     """
+    df = _df.copy()
     df['TR'] = np.maximum(df['high'] - df['low'], np.abs(df['high'] - df['close'].shift(1)))
     df['TR'] = np.maximum(df['TR'], np.abs(df['low'] - df['close'].shift(1)))
     df['ATR'] = df['TR'].ewm(span=period_atr).mean()
@@ -112,16 +113,16 @@ def apply_triple_bar_method(df, atr_mult=2.8, nr_of_bars_for_vertical=13,
     first = None
     for index, row in df.iterrows():
         if not first:
-            if row['bot_bar'] == 1:
+            if row['bot_bar']:
                 first = 'bot'
-            if row['top_bar'] == 1:
+            if row['top_bar']:
                 first = 'top'
-        if row['bot_bar'] + row['bot_bar'] == 0:
+        if not row['top_bar'] and not row['bot_bar']:
             first = None
-        if row['bot_bar'] + row['bot_bar'] > 1:
+        if row['top_bar'] and row['bot_bar']:
             if first == 'top':
-                df.loc[index, 'top_bar'] = 0
-            if first == 'bot':
                 df.loc[index, 'bot_bar'] = 0
-
+            else:
+                df.loc[index, 'top_bar'] = 0
     return df
+
